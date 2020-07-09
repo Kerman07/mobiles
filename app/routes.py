0342580b5a2f1d6c5bc2e1c00b2b732c64db60e0
Kerim Mandzo <kerman07@live.com>
2020-07-09 14:33:05 +0200

    Changed cart session handling

--- app/routes.py
+++ app/routes.py
@@ -50,12 +50,12 @@ def home():
                 phones = Phone.query.order_by(Phone.maker).all()
             elif 'newest' in session['sorting']:
                 phones = Phone.query.order_by(Phone.timestamp.desc()).all()
-        if 'cart' in request.form:
-            if request.form['cart'] not in session['cart']:
-                session['cart'].append(request.form['cart'])
         return redirect(url_for('home'))
     if 'sorting' not in session:
         phones = Phone.query.order_by(Phone.timestamp.desc()).all()
+    if 'cart' in request.form:
+        if request.form['cart'] not in session['cart']:
+            session['cart'].append(request.form['cart'])
     return render_template('index.html', phones=phones)
 
 
